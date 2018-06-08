6c0e434d7e5e9494ce1a00fb3f4034d2c885cac7
alex <alex@alex-HP-15-Notebook-PC>
2018-06-08 01:36:31 -0600

    Creacion de Menu

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ import Milicia.Soldado;
 import Vehiculos.FabricaVehiculos;
 import Vehiculos.TipoVehiculo;
 import Vehiculos.Vehiculo;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -20,6 +22,71 @@ import Vehiculos.Vehiculo;
 public class Main {
     
    public static void main(String []args){
+       
+ 
+
+ 
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opc1;
+        int opc2;
+ 
+        while (!salir) {
+ 
+            System.out.println("1. Crear Soldado");
+            System.out.println("2. Opcion 2");
+            System.out.println("3. Opcion 3");
+            System.out.println("4. Salir");
+ 
+            try {
+ 
+                System.out.println("Escribe una de las opciones");
+                opc1 = sn.nextInt();
+                opc2=0;
+ 
+                switch (opc1) {
+                    case 1:
+                        while(opc2!=4){
+            System.out.println("Soldado Creado");                
+            System.out.println("1.Atacar");
+            System.out.println("2. Opcion 2");
+            System.out.println("3. Opcion 3");
+            System.out.println("4. Salir");
+          
+            opc2=sn.nextInt();
+                                
+            switch(opc2){
+                        case 1:
+                        Jugador J1= new Jugador();
+                        System.out.println("");
+                        break;
+                        }
+                        }
+                    case 2:
+                        while(opc!=4){
+                        System.out.println("Has seleccionado la opcion 2");
+                        break;
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Has seleccionado la opcion 3");
+                        break;
+                    case 4:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 4");
+                }
+            }
+             catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                sn.next();
+            }
+        }
+ 
+    
+ 
+
        
        
            // crear un SER1
@@ -60,6 +127,8 @@ public class Main {
         
        
 }
+}
+}
        
-   }
+   
 
